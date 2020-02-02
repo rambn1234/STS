00d81b144c3288803053494cb602ddfa1f09858f
rambn1234 <rbnnagre@gmail.com>
2020-02-02 18:03:42 +0530

    rr

---
+++ src/main/java/com/cjc/StsRamDemo1Application.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsRamDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsRamDemo1Application.class, args);
+	}
+
+}
